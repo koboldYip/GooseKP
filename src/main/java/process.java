51fddc5b2f37224a4a28a35bd961b526c139ab9e
koboldYip <kyldoks@gmail.com>
2022-06-08 01:00:33 +0300

    For changed XML update process v1.1

--- src/main/java/process.java
+++ src/main/java/process.java
@@ -2,6 +2,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.SneakyThrows;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -12,15 +14,16 @@ public class process {
         XmlMapper xmlMapper = new XmlMapper();
         Root value = xmlMapper.readValue(new File("src/main/resources/Cfg.xml"), Root.class);
         ControlBlock controlBlock = new ControlBlock();
-        GOOSE gse = new GOOSE(value.getSenders().get(0));
-        controlBlock.getGooseList().add(gse);
+        List<GOOSE> gse = new ArrayList<>();
+        controlBlock.setGooseList(gse);
+        value.getSenders().forEach(dataSet -> gse.add(new GOOSE(dataSet)));
 
         Executors.newSingleThreadScheduledExecutor().schedule(
                 () -> {
                     value.getSenders().get(0).getItems().get(0).setValue("true");
                     value.getSenders().get(0).getItems().get(1).setValue("228");
                     value.getSenders().get(0).getItems().get(2).setValue("14.2");
-                    controlBlock.changeGoose(gse);
+                    controlBlock.changeGoose(gse.get(0));
                 }
                 , 10, TimeUnit.SECONDS);
 
